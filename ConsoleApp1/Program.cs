aa2a8ed21a71ed02d1142704abc3524ae9f12996
opilane <opilane@HP-CZC0359NP0>
2025-02-05 15:09:39 +0200

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿    namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double coinD = 25.75;
+            double earthR = 6378000000;
+            double aroundE = 2 * Math.PI * earthR;
+            Console.WriteLine("Maa ümbermõõt: " + aroundE);
+            double result = aroundE / coinD;
+            Console.WriteLine("Ümber maa mahub nii: " + "palju kahe eurosid" + result);
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
