73aae6c10fd5a3ae5dc77701948e89acbea1c890
opilane <opilane@HP-CZC0359NP0>
2025-02-05 15:50:03 +0200

    Sum
    13

---
+++ RandomColors/Program.cs
@@ -0,0 +1,36 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Suvalised värvid!");
+
+        // Генератор случайных чисел
+        Random random = new Random();
+        int randomNumber = random.Next(1, 6); // Случайное число от 1 до 5
+
+        switch (randomNumber)
+        {
+            case 1:
+                ConsoleColor consoleColor = ConsoleColor.Red;
+                Console.WriteLine("Valitud värv on Punane (Red).");
+                break;
+            case 2:
+                consoleColor = ConsoleColor.Green;
+                Console.WriteLine("Valitud värv on Roheline (Green).");
+                break;
+            case 3:
+                consoleColor = ConsoleColor.Blue;
+                Console.WriteLine("Valitud värv on Sinine (Blue).");
+                break;
+            case 4:
+                consoleColor = ConsoleColor.Magenta;
+                Console.WriteLine("Valitud värv on Valge (White).");
+                break;
+            case 5:
+                consoleColor = ConsoleColor.Yellow;
+                Console.WriteLine("Valitud värv on Oranž (Orange).");
+                break;
+        }
+    }
+}
---
+++ Switch/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta täht ja vajuta enter");
+            
+            string ch = Console.ReadLine();
+            switch (ch)
+            {
+                case "a":
+                    break;
+                case "e":
+                    Console.WriteLine("Vokaal a");
+                    break;
+
+                case "i":
+                    Console.WriteLine("Vokaal a");
+                    break;
+
+                default:
+                    Console.WriteLine("Ei ole vokaal e täishäälik");
+                    break;
+            }
+
+
+            }
+        }
+}
